cc2a7637680c393de26b3e7ae162b7ee8a1725aa
Adam Pennycuick <adampennycuick@gmail.com>
2016-10-16 09:51:21 +0100

    Second attempt at serving revisions

--- app.rb
+++ app.rb
@@ -12,4 +12,14 @@ class ServerApp < Sinatra::Base
     index_key = params[:index_key] || redis.get("#{project}:index:current")
     redis.get("#{project}:index:#{index_key}")
   end
+
+  get '/revisions/:index_key' do
+    content_type 'text/html'
+
+    settings = YAML.load_file('/var/www/redis_config.yml')
+    redis = Redis.new(host: settings["host"], port: settings["port"], password: settings["password"])
+    project = settings['project']
+    index_key = params[:index_key] || redis.get("#{project}:index:current")
+    redis.get("#{project}:index:#{index_key}")
+  end
 end
