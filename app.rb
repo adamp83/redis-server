83014a4d2805115c69093bd5e5f6b2f06352d482
Adam Pennycuick <adampennycuick@gmail.com>
2016-10-16 09:48:19 +0100

    Revert previous changes

--- app.rb
+++ app.rb
@@ -3,36 +3,13 @@ require 'redis'
 require 'yaml'
 
 class ServerApp < Sinatra::Base
-
   get '/' do
     content_type 'text/html'
 
     settings = YAML.load_file('/var/www/redis_config.yml')
     redis = Redis.new(host: settings["host"], port: settings["port"], password: settings["password"])
     project = settings['project']
-
     index_key = params[:index_key] || redis.get("#{project}:index:current")
     redis.get("#{project}:index:#{index_key}")
-  end
-
-  get '/:index_key' do
-    content_type 'text/html'
-
-    settings = YAML.load_file('/var/www/redis_config.yml')
-    redis = Redis.new(host: settings["host"], port: settings["port"], password: settings["password"])
-    project = settings['project']
-
-    index_key = params[:index_key] || redis.get("#{project}:index:current")
-    redis.get("#{project}:index:#{index_key}")
-  end
-
-  get '/revisions' do
-    content_type 'text/html'
-
-    settings = YAML.load_file('/var/www/redis_config.yml')
-    redis = Redis.new(host: settings["host"], port: settings["port"], password: settings["password"])
-    project = settings['project']
-
-    redis.get("#{project}:index:revisions")
   end
 end
